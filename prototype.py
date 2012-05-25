18663c04b889ddf3285fbf8b8bacc4d8f9a12499
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-05-25 23:39:17 +1000

    stock + backtest - starting to look funky

--- prototype.py
+++ prototype.py
@@ -5,9 +5,27 @@ import numpy
 from ystockquote import get_historical_prices
 
 
-today = datetime.date.today().strftime('%Y%m%d')
-goog = get_historical_prices('GOOG', '20010103', today)
+class Stock(object):
 
+    def __init__(self, symbol):
+        self.data = []
+        self._load(symbol)
+
+    def _load(self, symbol):
+        today = datetime.date.today().strftime('%Y%m%d')
+        raw = get_historical_prices(symbol, '20010103', today)
+        # Tick aware of the time series it belongs to
+        self.data.extend(
+            [ Tick(self.data, index, *tick)
+              for index, tick
+              in enumerate(reversed(raw[1:])) ])
+
+    def __iter__(self):
+        for tick in self.data:
+            yield tick
+
+    def __getitem__(self, index):
+        return self.data[index]
 
 class Strategy(object):
 
@@ -35,6 +53,7 @@ class Bollinger(Strategy):
 class Tick(namedtuple('Tick',
                       ['series', 'index', 'date', 'open', 'high', 'low',
                        'close', 'volume', 'adj'])):
+
     def std(self, n):
         index = self.index + 1
         return numpy.std([float(tick.close) for tick in self.series[index-n:index]])
@@ -46,15 +65,43 @@ class Tick(namedtuple('Tick',
     def trade(self, strategy):
         return strategy(self)
 
+class BackTest(object):
 
-# instantiating ticks
-ticks = []
-# extending ticks with Tick object pointing to ticks
-ticks.extend([Tick(ticks, index, *tick) for index, tick in enumerate(reversed(goog[1:]))])
+    sell = {'long': None, None: 'short'}
+    buy = {None: 'long', 'short': None}
 
-bol = Bollinger(20, 2)
+    def __call__(self, stock, strategy):
+        self.position = None
+        self.trades = []
+        self.latest = stock[-1]
+        for t in stock:
+            if t.trade(strategy) == 'buy' and self.position != 'long':
+                self.position = self.buy[self.position]
+                self.trades.append(float(t.close))
+            elif t.trade(strategy) == 'sell' and self.position != 'short':
+                self.position = self.sell[self.position]
+                self.trades.append(-float(t.close))
+        return self.position, self.pnl, float(self.latest.close), len(self.trades)
 
-tick = ticks[1050]
-print tick.trade(bol)
+    def cost(self, rate):
+        return sum(abs(trade) for trade in self.trades) * rate / 100.
+
+    @property
+    def pnl(self):
+        return -sum(self.trades)
+
+    def net(self, rate):
+        result = 0
+        if self.position == 'long':
+            result += self.latest.close
+        elif self.position == 'short':
+            result -= self.latest.close
+        result += self.pnl
+        result -= self.cost(rate)
+        return result
+
+bollinger = Bollinger(30, 1)
+goog = Stock('GOOG')
+backtest = BackTest()
 
 import code; code.interact(local=locals())
