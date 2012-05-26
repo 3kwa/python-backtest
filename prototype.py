8b78b2dbea6d225d170277d7d317b0009aa52493
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-05-26 20:54:31 +1000

    Tick __repr__

--- prototype.py
+++ prototype.py
@@ -100,10 +100,9 @@ class Tick(namedtuple('Tick',
     Contains a reference to the time series it belongs to (Stock.data) and its
     index in the list for metric computation.
 
-    >>> tick = Tick(None, None, datetime.date(2012, 5, 25), 1.0, 1.0, 1.0,
+    >>> Tick([], 0, datetime.date(2012, 5, 25), 1.0, 1.0, 1.0,
     ...              1.0, 1, 1.0)
-    >>> tick.date
-    datetime.date(2012, 5, 25)
+    Tick(series=[...], index=0, date=2012-05-25 open=1.0, high=1.0, low=1.0, close=1.0, volume=1, adj=1.0)
     """
 
     __slots__ = ()
@@ -115,6 +114,11 @@ class Tick(namedtuple('Tick',
     def ma(self, n):
         index = self.index + 1
         return numpy.mean([tick.close for tick in self.series[index-n:index]])
+
+    def __repr__(self):
+        return 'Tick(series=[...], index={0.index}, date={0.date} \
+open={0.open}, high={0.high}, low={0.low}, close={0.close}, \
+volume={0.volume}, adj={0.adj})'.format(self)
 
 
 class BackTest(object):
@@ -155,4 +159,4 @@ class BackTest(object):
 bollinger = Bollinger(30, 1)
 backtest = BackTest()
 
-import code; code.interact(local=locals())
+#import code; code.interact(local=locals())
