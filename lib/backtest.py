7e7b5238b68cc9dcc24ac748939ce90369d24cc7
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-05-28 12:30:39 +1000

    backtest doc

--- lib/backtest.py
+++ lib/backtest.py
@@ -1,4 +1,26 @@
 class BackTest(object):
+    """ Callable object running back tests for a strategy over a stock
+
+    >>> backtest = BackTest()
+
+    If goog is a Stock and bollinger a strategy (cf. strategy/__init__.py)
+
+    >>> backtest(goog, bollinger)
+    BackTest(trades=[99], position=short, gross=1253.63, net=662.1, passive=491.19)
+
+    Current position is short, gross PNL is 1253.63, net PNL taking into account
+    the closing of the position and the trading costs if applicable is 662.1.
+
+    A passive strategy (buy on day 1 sell on last day) would have returned 491.19
+
+    Trading costs are 0 by default. To change that set the cost attribute of
+    the backtest object to a function taking a trade as an argument and returning
+    the cost.
+
+    >>> backtest.cost = lambda trade: 0.5 * trade / 100
+    >>> backtest
+    BackTest(trades=[99], position=short, gross=1253.63, net=435.78005, passive=491.19)
+    """
 
     sell = {'long': None, None: 'short'}
     buy = {None: 'long', 'short': None}
@@ -26,6 +48,7 @@ net={0.net}, passive={0.passive})'.format(self, len(self.trades))
 
     @property
     def trade_cost(self):
+        """ return the cost trading cost over the backtesting period """
         return sum(self.cost(abs(trade)) for trade in self.trades)
 
     @property
@@ -45,5 +68,5 @@ net={0.net}, passive={0.passive})'.format(self, len(self.trades))
 
     @property
     def passive(self):
+        """ buy on day 1 sell on last day """
         return self.last.close - self.first.close
-
